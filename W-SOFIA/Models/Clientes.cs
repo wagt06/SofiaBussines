db6d8620d1a623fe34b3a7dd98ba85c41498f872
william gonzalez tellez <wagt2006@hotmail.com>
2018-09-16 18:20:14 -0600

    datavalidations Clientes

--- W-SOFIA/Models/Clientes.cs
+++ W-SOFIA/Models/Clientes.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace W_SOFIA.Models
 {
@@ -9,18 +11,44 @@ namespace W_SOFIA.Models
         {
             Documentos = new HashSet<Documentos>();
         }
-
+        [NotMapped]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CodigoCliente { get; set; }
+
+        [Required(ErrorMessage ="El campo Nombre es Necesario")]
+        [MaxLength(20, ErrorMessage = "Nombre del Cliente must be 10 characters or less"), MinLength(10)]
         public string NombreCliente { get; set; }
+
+        [Required]
+        [MaxLength(10, ErrorMessage = "BloggerName must be 10 characters or less"), MinLength(5)]
         public string NoIdentificacion { get; set; }
+
+        [Required]
+        [MaxLength(10, ErrorMessage = "BloggerName must be 10 characters or less"), MinLength(5)]
         public string Telefono { get; set; }
+
+        [Required]
+        [MaxLength(10, ErrorMessage = "BloggerName must be 10 characters or less"), MinLength(5)]
         public string Celular { get; set; }
+
+        [Required]
+        [MaxLength(10, ErrorMessage = "BloggerName must be 10 characters or less"), MinLength(5)]
         public string Email { get; set; }
+        [Required]
+        [MaxLength(10, ErrorMessage = "BloggerName must be 10 characters or less"), MinLength(5)]
         public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "BloggerName must be 10 characters or less")]
         public int? CodPais { get; set; }
+        [Required]
         public int? CodMunicipio { get; set; }
+
+        [Required]
         public int? CodDepartamento { get; set; }
+
+        [Required]
         public string DireccionEnvio { get; set; }
+
         public DateTime? FechaAlta { get; set; }
         public string NumeroCuenta { get; set; }
         public int? CodigoTerminosPago { get; set; }
